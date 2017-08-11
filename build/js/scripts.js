0fb5a6883a7b0f24bca6f7e78ef473ee529a2e33
Gabriel Grieve <gabe@innuendodigital.com.au>
2017-08-11 11:02:30 +1000

    Added opening hours functionality and style

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -10,3 +10,11 @@ $( document ).ready(function() {
 
 $(".header-navigation > ul > li:has(ul)").addClass("has-sub-menu");
 $(".has-sub-menu > a").append("<i class='fa fa-caret-down inline-icon' aria-hidden='true'></i>");
+
+$(".opening-times-container").click(function(){
+	$(".opening-times-pop-out").toggleClass("open");
+});
+
+$(".opening-times-pop-out .close").click(function(){
+	$(".opening-times-pop-out").removeClass("open");
+});
