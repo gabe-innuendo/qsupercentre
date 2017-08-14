d412cdd34f148aae680fbf17b2faf7220a2347c1
Gabriel Grieve <gabe@innuendodigital.com.au>
2017-08-14 10:47:35 +1000

    Added in unslider files and put fixed positioning on header-menu

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,14 +1,15 @@
 $( document ).ready(function() {
 	
+	$('.front-page-slider').unslider({
+		arrows: false,
+		autoplay: true,
+		infinite: true
+	});
+
 });
 
-// Append dropdown indicator to menu items with submenus
-
-// Target header-navigation element 
-// Find any li item with a ul child
-// Append the fontawesome caret down icon to the li element
-
 $(".header-navigation > ul > li:has(ul)").addClass("has-sub-menu");
+
 $(".has-sub-menu > a").append("<i class='fa fa-caret-down inline-icon' aria-hidden='true'></i>");
 
 $(".opening-times-container").click(function(){
